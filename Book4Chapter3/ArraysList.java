6a28cc66dd17b65e3bd229f7501f7a55fe9d22de
SechabaMadyibi <sechabamadyibi1@gmail.com>
2024-01-29 21:44:19 +0200

    update

---
+++ Book4Chapter3/ArraysList.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Book4Chapter3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dmtshoeni
+ */
+public class ArraysList {
+    public static void main(String[] args) {
+        ArrayList<Employee> emps = new ArrayList<Employee>();
+// create employee objects
+Employee emp1 = new Employee("Addams", "Gomez");
+Employee emp2 = new Employee("Taylor", "Andy");
+Employee emp3 = new Employee("Kirk", "James");
+// add employee objects to array list
+emps.add(emp1);
+emps.add(emp2);
+emps.add(emp3);
+// print the array list
+System.out.println(emps);
+// remove one of the employees
+emps.remove(emp2);
+// print the array list again
+System.out.println(emps);
+    }
+}
+
---
+++ Book4Chapter5/GenQueue.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Book4Chapter5;
+
+/**
+ *
+ * @author dmtshoeni
+ */
+//import java.util.*;
+//
+//public class GenQueue {
+//
+//    private LinkedList<E> list = new LinkedList<E>();
+//
+//    public void enqueue(E item) {
+//        list.addLast(item);
+//    }
+//
+//    public E dequeue() {
+//        return list.poll();
+//    }
+//
+//    public boolean hasItems() {
+//        return !list.isEmpty();
+//    }
+//
+//    public int size() {
+//        return list.size();
+//    }
+//
+//    public void addItems(GenQueue<? extends E> q) {
+//        while (q.hasItems()) {
+//            list.addLast(q.dequeue());
+//        }
+//    }
+//}
